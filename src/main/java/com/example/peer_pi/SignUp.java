dc1cea5343cb635602e8d1c5e4cd19ba8f980313
Aranya Kishor Das <43179202+aranya170@users.noreply.github.com>
2023-05-01 13:15:19 +0600

    Add files via upload

---
+++ src/main/java/com/example/peer_pi/SignUp.java
@@ -0,0 +1,27 @@
+package com.example.peer_pi;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+public class SignUp extends Controller{
+    @FXML
+    private MFXTextField LastName;
+
+    @FXML
+    private MFXButton LogInBtn;
+
+    @FXML
+    private MFXButton LogInBtn1;
+
+    @FXML
+    private MFXTextField UID;
+
+    @FXML
+    private MFXTextField firstName;
+
+    @FXML
+    private MFXTextField mailBtn1;
+
+    @FXML
+    private MFXTextField passBtn1;
+}
